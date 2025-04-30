fb177e4d230ede6c35103e739131a793a343c944
이정준 <ljj10571@gmail.com>
2025-05-01 01:20:47 +0900

    미션5

---
+++ src/main/java/umc/spring/domain/mapping/MemberMission.java
@@ -0,0 +1,37 @@
+package umc.spring.domain.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.Member;
+import umc.spring.domain.Mission;
+import umc.spring.domain.base.BaseEntity;
+import umc.spring.domain.enums.MissionStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'CHALLENGING'")
+    private MissionStatus missionStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member.id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission.id")
+    private Mission mission;
+
+
+}
