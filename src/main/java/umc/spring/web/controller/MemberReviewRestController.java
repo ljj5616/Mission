4b0c504a89a91110f62d70e062190b47978033c0
이정준 <ljj10571@gmail.com>
2025-06-17 16:20:13 +0900

    add MemberReviewRestController

---
+++ src/main/java/umc/spring/web/controller/MemberReviewRestController.java
@@ -0,0 +1,48 @@
+package umc.spring.web.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.converter.MemberReviewConverter;
+import umc.spring.domain.Review;
+import umc.spring.service.memberService.MemberQueryService;
+import umc.spring.validation.annotation.CheckPage;
+import umc.spring.web.dto.MemberResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@RequestMapping("/members")
+@Slf4j
+public class MemberReviewRestController {
+
+    private final MemberQueryService memberQueryService;
+
+    @GetMapping("/{memberId}/reviews")
+    @Operation(summary = "특정 회원의 리뷰 목록 조회 API", description = "특정 회원의 리뷰 목록을 조회하는 API이며, 페이징을 포함한다. query string으로 page 번호 전달")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "access 토큰 만료", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "access 토큰 모양이 이상함", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "memberId", description = "회원의 아이디, path variable 입니다!"),
+            @Parameter(name = "page", description = "조회할 page 번호 입니다.")
+    })
+
+    public ApiResponse<MemberResponseDTO.MyReviewListDTO> getReviewList(@PathVariable(name = "memberId") Long memberId, @CheckPage @RequestParam(name = "page") Integer page) {
+        log.info("page={}", page);
+        Page<Review> reviewList = memberQueryService.getMyReviews(memberId, page);
+        return ApiResponse.onSuccess(MemberReviewConverter.toMyReviewListDTO(reviewList));
+    }
+}
